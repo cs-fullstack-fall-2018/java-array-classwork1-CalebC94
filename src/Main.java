fecf6f1a101e2d567c445550c8689b5c6123cc46
Caleb Canada <calebcanada13@gmail.com>
2018-07-24 13:04:44 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> squad = new ArrayList<String>();
+        squad.add();
+        squad.add();
+        squad.add();
+        squad.add();
+        
+    }
+}
